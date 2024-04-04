a847eb9bef13677f7db327d151725880a5c56c13
KLEugene <kaeugene95@gmail.com>
2024-04-04 19:42:45 +0100

    finished modal window

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+const refModal = document.querySelector(".modal");
+const refBtnClose = document.querySelector(".button-close");
+const refBtnSubmit = document.querySelector(".modal__button");
+const refBtnOpen =document.querySelector("[data-modal-open]")
+
+refBtnClose.addEventListener("click", onClick);
+refBtnSubmit.addEventListener("click", onClick);
+refBtnOpen.addEventListener("click", onClick);
+function onClick() { 
+    refModal.classList.toggle("is-hidden");
+};
+
